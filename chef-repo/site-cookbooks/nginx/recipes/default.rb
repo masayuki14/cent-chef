c7a4200bb31be561795e013d330b372488ab1171
Masayuki Morisaki <masayuki144@gmail.com>
2015-01-05 16:28:21 +0900

    nginxのレシピを更新 * 起動時にstart * 設定ファイルをテンプレートから設置

--- chef-repo/site-cookbooks/nginx/recipes/default.rb
+++ chef-repo/site-cookbooks/nginx/recipes/default.rb
@@ -5,8 +5,24 @@
 # Copyright 2015, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
-#
+
 package 'nginx' do
   action :install
 end
 
+# nginx の起動
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+# 設定ファイルの設置
+# site-cookbooks/nginx/templates/default/nginx.conf.erb が利用される
+template 'nginx.conf' do
+  path '/etc/nginx/nginx.conf'
+  owner 'root'
+  group 'root'
+  mode 0644
+
+  # ファイルが更新されたらサービスをリロード
+  notifies :reload, 'service[nginx]'
+end
