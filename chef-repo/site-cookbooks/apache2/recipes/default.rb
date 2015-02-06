b66082e782e066dd5979559b52753b8818a925b4
Masayuki Morisaki <masayuki144@gmail.com>
2015-02-06 10:13:38 +0900

    apache2レシピを整形

--- chef-repo/site-cookbooks/apache2/recipes/default.rb
+++ chef-repo/site-cookbooks/apache2/recipes/default.rb
@@ -28,27 +28,30 @@ service web_server do
   action [ :enable, :start ]
 end
 
-if node[:platform] == 'ubuntu'
+case node[:platform]
+when 'ubuntu'
   # VirtualHost の設定ファイルを作成し有効化してリロード
   template 'dev.hybrid.local.conf' do
-    path  '/etc/apache2/sites-available/dev.hybrid.local.conf'
-    owner 'root'
+    path     '/etc/apache2/sites-available/dev.hybrid.local.conf'
+    owner    'root'
     notifies :run, 'execute[a2ensite_dev.hybrid.local]'
 
   end
 
   execute 'a2ensite_dev.hybrid.local' do
-    action :nothing
-    user 'root'
-    command 'a2ensite dev.hybrid.local'
+    action   :nothing
+    user     'root'
+    command  'a2ensite dev.hybrid.local'
     notifies :restart, 'service[apache2]'
   end
 
   # apache module の適用
   execute 'a2enmod' do
-    user 'root'
-    command 'a2enmod rewrite'
+    user     'root'
+    command  'a2enmod rewrite'
     notifies :restart, 'service[apache2]'
   end
+
+when 'centos'
 
 end
